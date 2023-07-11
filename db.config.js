ad61b61a7e51ee2ac9ab7761728136d02aa0a511
Devika <71765863+devikaupv@users.noreply.github.com>
2023-07-12 00:17:02 +0530

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,6 +1,3 @@
-const dbName = 'food-ordering';
-const dbHost = 'localhost';
-const dbPort = 27017;
 module.exports = {
-    url: `mongodb://${dbHost}:${dbPort}/${dbName}`
+    url: "mongodb+srv://shindemaanav:uZogViP58lqdaHxk@cluster0.uvizoia.mongodb.net/"
 }
