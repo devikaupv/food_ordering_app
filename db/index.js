b1b052cda3af69c5db7e6cf6a6df446f8fc437ba
Devika <71765863+devikaupv@users.noreply.github.com>
2023-07-12 00:14:07 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://localhost:27017/food-ordering', { useNewUrlParser: true })
+    .connect('mongodb+srv://shindemaanav:uZogViP58lqdaHxk@cluster0.uvizoia.mongodb.net/', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
 const db = mongoose.connection
-db.once('open', () => {
-    console.log('Connected to the database');
-    // Your additional code or operations can go here
-  });
-  
-  // Handle connection errors
-  db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-  });
+
 module.exports = db
