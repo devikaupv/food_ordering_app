f10c7833ac8204aa310b8edabd07621a4cfe45b3
Devika <71765863+devikaupv@users.noreply.github.com>
2023-07-12 00:16:08 +0530

    Update productRouter.js

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
+const express = require('express')
+
+const router = express.Router()
+
 const Product = require('../models/productModel')
 
 router.get('/products', async (req, res) => {
